6be482da5ab34f7857a28d163a5f99b33268a4bc
kevin <52811426+KevinPeng041@users.noreply.github.com>
2020-07-17 09:28:23 +0800

    Create NISPAPICNBD.php

---
+++ NIS/CNBD/NISPAPICNBD.php
@@ -0,0 +1,16 @@
+<?php
+function GetCNBDIniJson(){
+    //取護理站初始清單
+}
+function GetCNBDPageJson(){
+    //取護理站輸血血袋紀錄清單
+}
+function PosCNBDSave(){
+    //血袋領用簽收資料儲存
+}
+function GetCBLDJson(){
+    //產生已儲存之紀錄資料
+}
+function PosCNBDCancel(){
+   //作廢領用血簽收紀錄
+}
