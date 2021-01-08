a355fea8c59df23afe722e64e1426dbdda4229c9
kevin <52811426+KevinPeng041@users.noreply.github.com>
2021-01-08 08:49:39 +0800

    Update NISPRWCNAD.php
    Serch (查詢) to sfm

--- NIS/CNAD/NISPRWCNAD.php
+++ NIS/CNAD/NISPRWCNAD.php
@@ -117,7 +117,7 @@ $From=trim($From_value[1]);/*L:登入介面,U:URL操作*/
                                     AESEnCode("sFm=CNAD&PageVal="+""+"&DA_idpt="+
                                         $('#DA_IdPt').val()+"&DA_idinpt="+""+
                                         "&sUser="+"<?php echo $OPID?>"+"&NM_PATIENT="+"")
-                                    ,"查詢",'width=750px,height=650px,scrollbars=yes,resizable=no');
+                                    ,"CNAD",'width=750px,height=650px,scrollbars=yes,resizable=no');
                                 break;
                         }
 
