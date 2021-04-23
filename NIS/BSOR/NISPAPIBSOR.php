4d59918522f690bedaad12ab9394a0359c8c1c06
kevin <52811426+KevinPeng041@users.noreply.github.com>
2021-04-23 18:09:36 +0800

    Update NISPAPIBSOR.php

--- NIS/BSOR/NISPAPIBSOR.php
+++ NIS/BSOR/NISPAPIBSOR.php
@@ -2,4 +2,17 @@
 
 function GetBSORIniJson($conn,$Idpt,$INPt,$ID_BED,$sTraID,$sSave,$date,$sUr,$JID_NSRANK,$FORMSEQANCE_WT){
 
+    $SQL="SELECT ST_PREA,ST_PREB,ST_PREC FROM NISWSIT WHERE ID_TABFORM='BSOR'";
+
+    $stid=oci_parse($conn,$SQL);
+    oci_execute($stid);
+    $arr='';
+    while (oci_fetch_array($stid)){
+        $MM_TEXT=oci_result($stid,"ST_PREA")->load();
+        $Tittle_Nm=oci_result($stid,"ST_PREB")->load();
+        $Tittle_CNm=oci_result($stid,"ST_PREC")->load();
+
+        $arr=array("MM_TEXT"=>json_decode($MM_TEXT),"Tittle_Nm"=>json_decode($Tittle_Nm),"Tittle_CNm"=>json_decode($Tittle_CNm));
+    }
+    return json_encode($arr,JSON_UNESCAPED_UNICODE);
 }
